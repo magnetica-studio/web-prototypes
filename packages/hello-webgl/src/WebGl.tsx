bba8e67f4d581c71bff8f6d5696b8a3006c2017e
tomoki midorikawa <46930312+Yasai-T@users.noreply.github.com>
2019-12-22 02:10:52 +0900

    refactor: WebGL

--- packages/hello-webgl/src/WebGl.tsx
+++ packages/hello-webgl/src/WebGl.tsx
@@ -2,31 +2,19 @@ import React, { useEffect } from "react";
 
 export const WebGl = () => {
   useEffect(() => {
-    const canvas = document.getElementById("webgl")
-    
-    const initWebGL = (canvas: HTMLCanvasElement) => {
-      let gl: WebGLRenderingContext | null = null
-      try {
-        gl = canvas.getContext("webgl")
-      } catch (e) {}
-      if (!gl) {
-        alert(
-          "WebGL を初期化できません。ブラウザはサポートしていないようです。"
-        );
-        gl = null;
+    const canvas = document.querySelector("canvas")
+    if (canvas) {
+      const gl = canvas.getContext('webgl')
+      if (gl) {
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.enable(gl.DEPTH_TEST);
+        gl.depthFunc(gl.LEQUAL);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
       }
-      return gl;
-    };
-
-    const gl = initWebGL(canvas as HTMLCanvasElement)
-    if (gl) {
-      gl.clearColor(0.0, 0.0, 0.0, 1.0);
-      gl.enable(gl.DEPTH_TEST);
-      gl.depthFunc(gl.LEQUAL);
-      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+      console.log('canvas: ', canvas)
+      console.log('WebGLRenderingContext: ', gl)
     }
-    console.log('render canvas: ', gl)
   }, []);
 
-  return <canvas id="webgl" width="400" height="400"></canvas>;
+  return <canvas width="400" height="400"></canvas>;
 };
